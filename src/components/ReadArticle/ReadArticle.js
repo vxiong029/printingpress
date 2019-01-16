07f49594589041fa877f1604fc3e1f3d11c3ef9f
Vang Xiong <vxiong029@gmail.com>
2019-01-16 09:34:02 -0600

    TextEditor.js: Data renders properly to state

--- src/components/ReadArticle/ReadArticle.js
+++ src/components/ReadArticle/ReadArticle.js
@@ -12,8 +12,8 @@ import {
 } from 'draft-js';
 
 // component imports
-import DeleteArticleButton from '../DeleteArticleButton/DeleteArticleButton';
 import FollowButton from '../FollowButton/FollowButton';
+import TextEditor from '../TextEditor/TextEditor';
 
 class ReadArticle extends Component {
   componentDidMount() {
@@ -22,73 +22,7 @@ class ReadArticle extends Component {
       payload: this.props.match.params.id,
     })
   }
-
-  constructor(props) {
-    super(props);
-    if (!this.props.readArticle.blog_content) {
-      this.state = {
-        editorState: EditorState.createEmpty()
-      };
-      console.log('empty');
-    } else {
-      const rawContent = this.props.readArticle.blog_content;
-      const parsedContent = convertFromRaw(JSON.parse(rawContent));
-
-      this.state = {
-        editorState: EditorState.createWithContent(parsedContent)
-      };
-      console.log('not empty');
-    }
-  }
-  // edit button handle click
-  handleEdit = (id) => {
-    console.log('in handle edit', id);
-
-    this.props.dispatch({
-      type: 'EDIT_ARTICLE',
-      payload: id
-    })
-  }
-  // handle content change
-  handleContentChange = (editorState) => {
-    this.setState({
-      editorState
-    });
-  }
   render() {
-    let content;
-    if (this.props.user.full_name === this.props.readArticle.full_name) {
-      content =
-        <div>
-          <Editor
-            editorState={this.state.editorState}
-            onChange={this.handleContentChange}
-            // plugins={plugins}
-            ref={(element) => { this.editor = element; }}
-            data={this.props.readArticle.blog_content}
-          />
-          <p>
-            <button onClick={() => this.handleEdit(this.props.readArticle.id)}>Save Edit</button>
-            <Link to="/articles">
-              <DeleteArticleButton
-                postId={this.props.readArticle.id}
-              />
-            </Link>
-          </p>
-        </div>
-    } else {
-      content =
-        <div>
-          <Editor
-            editorState={this.state.editorState}
-            onChange={this.handleContentChange}
-            // plugins={plugins}
-            ref={(element) => { this.editor = element; }}
-            data={this.props.readArticle.blog_content}
-            readOnly
-          />
-        </div>
-    }
     return (
       <div key={this.props.readArticle.id}>
         <h2>{this.props.match.params.id}</h2>
@@ -102,7 +36,9 @@ class ReadArticle extends Component {
         <p>Date: {this.props.readArticle.date}</p>
         <p>Author: {this.props.readArticle.full_name}</p>
         <p>{this.props.readArticle.description}</p>
-        {content}
+        <TextEditor 
+          article={this.props.readArticle.blog_content}
+        />
       </div>
     )
   }
--- src/components/TextEditor/TextEditor.js
+++ src/components/TextEditor/TextEditor.js
@@ -3,150 +3,108 @@ import { connect } from 'react-redux';
 // css import
 import '../CreatePost/CreatePost.css';
 import 'draft-js-focus-plugin/lib/plugin.css';
+// router imports
+import { Link } from 'react-router-dom';
+
+import DeleteArticleButton from '../DeleteArticleButton/DeleteArticleButton';
+
 // draft.js imports
 import { 
   EditorState, 
   convertToRaw,
-  // convertFromRaw
+  convertFromRaw
 } from 'draft-js';
 // allows for plugins to work
 import Editor from 'draft-js-plugins-editor';
 // plugin imports
 import createFocusPlugin from 'draft-js-focus-plugin';
 import createToolbarPlugin, { Separator } from 'draft-js-static-toolbar-plugin';
-import {
-  ItalicButton,
-  BoldButton,
-  UnderlineButton,
-  CodeButton,
-  UnorderedListButton,
-  OrderedListButton,
-  BlockquoteButton,
-} from 'draft-js-buttons';
-
-// plugins objects
-const focusPlugin = createFocusPlugin();
-const staticToolbarPlugin = createToolbarPlugin();
-const { Toolbar } = staticToolbarPlugin;
-const plugins = [focusPlugin, staticToolbarPlugin];
-
 // import {
-//   MegadraftEditor,
-//   editorStateFromRaw,
-//   editorStateToJSON
-// } from "megadraft";
+//   ItalicButton,
+//   BoldButton,
+//   UnderlineButton,
+//   CodeButton,
+//   UnorderedListButton,
+//   OrderedListButton,
+//   BlockquoteButton,
+// } from 'draft-js-buttons';
 
-// import image from "megadraft/lib/plugins/image/plugin";
-
-// import 'megadraft/dist/css/megadraft.css';
+// // plugins objects
+// const focusPlugin = createFocusPlugin();
+// const staticToolbarPlugin = createToolbarPlugin();
+// const { Toolbar } = staticToolbarPlugin;
+// const plugins = [focusPlugin, staticToolbarPlugin];
 
 class TextEditor extends Component {
-  // empty state of blog post
-  state = {
-    newPost: {
-      img_header: '',
-      title: '',
-      date: '',
-      category_id: 1,
-    },
-    editorState: EditorState.createEmpty(),
-  //   editorState: editorStateFromRaw(this.props.readArticle.blog_content)
+  constructor(props) {
+    super(props);
+    if (!this.props.readArticle.blog_content) {
+      this.state = {
+        editorState: EditorState.createEmpty()
+      };
+      console.log('empty');
+    } else {
+      const rawContent = this.props.readArticle.blog_content;
+      const parsedContent = convertFromRaw(JSON.parse(rawContent));
+
+      this.state = {
+        editorState: EditorState.createWithContent(parsedContent)
+      };
+      console.log('not empty');
+    }
   }
-  // holds the post info
-  handleDetailChange = propertyName => (event) => {
-    this.setState({
-      newPost: {
-        ...this.state.newPost,
-        [propertyName]: event.target.value,
-      }
-    });
+  // edit button handle click
+  handleEdit = (id) => {
+    console.log('in handle edit', id);
+
+    this.props.dispatch({
+      type: 'EDIT_ARTICLE',
+      payload: id
+    })
   }
+  // handle content change
   handleContentChange = (editorState) => {
     this.setState({
       editorState
     });
   }
-  // trigger saga post
-  submitPost = () => {
-    const content = convertToRaw(this.state.editorState.getCurrentContent());
-    // Your function to save the content
-    // save_my_content(content);
-    console.log(content);
-
-    this.props.dispatch({
-      type: 'POST_ARTICLE',
-      payload: {
-        post_content: content,
-        post_details: this.state.newPost
-      }
-    });
-  }
-  // focus on an object
-  // focus = () => {
-  //   this.editor.focus();
-  // }
   render() {
-    return (
-      <div>
-        {/* <pre>{JSON.stringify(this.state.newPost)}</pre> */}
-        <div className="toolbar">
-          <input
-            type="url"
-            placeholder="Article Image"
-            onChange={this.handleDetailChange('img_header')}
-          />
-          <input
-            type="text"
-            placeholder="Title"
-            onChange={this.handleDetailChange('title')}
-          />
-          <input
-            type="date"
-            placeholder="Date"
-            onChange={this.handleDetailChange('date')}
-          />
-          <select onChange={this.handleDetailChange('category_id')}>
-            <option value={1}>React</option>
-            <option value={2}>CSS</option>
-            <option value={3}>HTML</option>
-            <option value={4}>Javascript</option>
-            <option value={5}>Technology</option>
-          </select>
-          <Toolbar>
-            {
-              // may be use React.Fragment instead of div to improve perfomance after React 16
-              (externalProps) => (
-                <div>
-                  <BoldButton {...externalProps} />
-                  <ItalicButton {...externalProps} />
-                  <UnderlineButton {...externalProps} />
-                  <CodeButton {...externalProps} />
-                  <Separator {...externalProps} />
-                  <UnorderedListButton {...externalProps} />
-                  <OrderedListButton {...externalProps} />
-                  <BlockquoteButton {...externalProps} />
-                </div>
-              )
-            }
-          </Toolbar>
-        </div>
-        <div className="editor">
+    let content;
+    if (this.props.user.full_name === this.props.readArticle.full_name) {
+      content =
+        <div>
           <Editor
             editorState={this.state.editorState}
             onChange={this.handleContentChange}
-            plugins={plugins}
+            // plugins={plugins}
             ref={(element) => { this.editor = element; }}
+            data={this.props.readArticle.blog_content}
           />
-
-            {/* <MegadraftEditor
-              editorState={this.state.editorState}
-              onChange={this.handleContentChange}
-              plugins={[image]}
-            /> */}
+          <p>
+            <button onClick={() => this.handleEdit(this.props.readArticle.id)}>Save Edit</button>
+            <Link to="/articles">
+              <DeleteArticleButton
+                postId={this.props.readArticle.id}
+              />
+            </Link>
+          </p>
         </div>
+    } else {
+      content =
         <div>
-          <button onClick={this.submitPost}>Submit</button>
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.handleContentChange}
+            // plugins={plugins}
+            ref={(element) => { this.editor = element; }}
+            data={this.props.readArticle.blog_content}
+            readOnly
+          />
         </div>
+    }
+    return (
+      <div>
+        {content}
       </div>
     )
   }
